1df3b9b31136a90e9f9ef2282a3e019e34faff00
test-github-04 <nastenka.grigoreva@gmail.com>
2024-10-14 22:52:09 +0500

    first commit

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,38 @@
+package web.model;
+
+
+public class Car {
+    public String model;
+    private int price;
+    int year;
+
+    public Car(String model, int price, int year) {
+        this.model = model;
+        this.price = price;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
